8bf7f4d5ecaabd5a6d905217cce2a3416612f95b
Jerry Cui <125308969+jerrycui2007@users.noreply.github.com>
2024-10-17 22:46:39 -0400

    ?????

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,11 @@
 module com.wikijournalfx.wikijournalfx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.web;
     requires com.google.gson;
+
+
+
 
     opens com.wikijournalfx.wikijournalfx to javafx.fxml, com.google.gson;
     exports com.wikijournalfx.wikijournalfx;
